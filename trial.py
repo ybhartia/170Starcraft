49127db28501ba3c7fbf1c0aaa6416de6fce7be8
YashBHartia <yash@bhartia.in>
2018-05-14 12:29:33 -0700

    created testing scenerio

--- trial.py
+++ trial.py
@@ -4,25 +4,43 @@ import inputSVM as input
 import math
 
 
+#
+# Collects data from inputSVM
+#
 X, Y = input.generateInput('replayCollection.txt')
+
+#
+# Calculates the values used to split the data into training and testing
+#
 total = len(X)
 trainLen = 0.75 * total
-print total
-print int(trainLen)
+
+#
+# Split the data into training and testing based on values above
+#
 trainingX = X[0:int(trainLen)]
 trainingY = Y[0:int(trainLen)]
-
 testingX = X[int(trainLen):]
 testingY = Y[int(trainLen):]
 
-print(len(trainingX), len(trainingY))
-print(len(testingX), len(testingY))
-
-
+#
+# Trains all our models
+#
 svmImplem.trainReplays(trainingX,trainingY)
+
+#
+# picks the best model and gets the score for that model
+#
 bestModel, bestScore = svmImplem.pickBestModel(testingX,testingY)
 
+#
+# Creates a test case to check if the test case wins
+#
 newInput = [123, 123, 2341, 13241, 13451343, 12, 345, 123, 5, 12354]
+
+#
+# Checks if the test case wins
+#
 print(svmImplem.checkForWin(newInput,bestModel))
 
 
