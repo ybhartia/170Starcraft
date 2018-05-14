ee1e8d4e1697b3faed152ca957dedd5bba16306d
YashBHartia <yash@bhartia.in>
2018-05-14 11:22:35 -0700

    input file generated

--- inputSVM.py
+++ inputSVM.py
@@ -1,17 +1,32 @@
-filepath = 'replayCollection.txt'  
-with open(filepath) as fp:  
-   line = fp.readline()
-   cnt = 1
-   while line:
-       # print("Line {}: {}".format(cnt, line.strip()))
-       line = line.strip()
-       vals = line.split(" ")
-       for i in range(0,len(vals)-1):
-       	vals[i] = int(vals[i])
-
-
-       print(vals)
-       # READ THE NEXT LINE
+import numpy as np
+def generateInput(filepath):
+# filepath = 'replayCollection.txt'
+    x = [[]]
+    with open(filepath) as fp:
        line = fp.readline()
-       cnt += 1
-      
+       cnt = 1
+
+       while line:
+
+           # print("Line {}: {}".format(cnt, line.strip()))
+           line = line.strip()
+           vals = line.split(" ")
+
+           # if cnt == 1:
+           #     x = np.array(len(vals))
+
+
+           for i in range(0,len(vals)):
+            vals[i] = int(vals[i])
+
+           # if cnt == 1:
+           #     x = np.array(vals)
+           # else:
+           x += [vals]
+           # READ THE NEXT LINE
+           line = fp.readline()
+           cnt += 1
+
+    return x[1:]
+
+input = generateInput('replayCollection.txt')
