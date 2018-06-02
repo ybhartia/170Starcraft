38a50b6aee17eb73952c0106ec345a44bd9b6ef0
YashBHartia <yash@bhartia.in>
2018-06-01 20:03:09 -0700

    comments in Yash n Bhalla 50%

--- master.py
+++ master.py
@@ -2,21 +2,31 @@ from __future__ import print_function
 import sc2reader
 from sc2reader.engine.plugins import APMTracker
 
-# format of the vector
-
+#
+# This is the currently completed format of the input layer
 # [time, isProtoss, isZerg, isTerran, didBuildUnit, , , , , result]
+#
 
+#Replay location
 myReplay = '/Users/sakshambhalla/Downloads/ggtracker_93731.SC2Replay'
 
+#
+# The player data that is used to initialize the input vectors
+#
 class Player:
     def __init__(self):
-        self.teamid = ""
-        self.playerName = ""
-        self.protoss = 0
-        self.zerg = 0
-        self.terran = 0
-        self.result = -1
+        self.teamid = "" # Probably an int
+        self.playerName = "" # String
+        self.protoss = 0 # 0 or 1
+        self.zerg = 0 # 0 or 1
+        self.terran = 0 # 0 or 1
+        self.result = -1 # 0 or 1, if -1 then it has not been initialized or was not found
 
+
+#
+# Temporary debugger function:
+# Helps see player data
+#
 def printPlayers(players):
     for player in players:
         print("____________________________________")
@@ -30,6 +40,12 @@ def printPlayers(players):
     print("____________________________________")
 
 
+#
+# Reads basic data from the players array and initializes their values
+# We want these values to be accurate and these values are constant 
+# Through out the code so we do not need to change these values
+# EVER
+#
 def initiatePlayers(replay):
     players = []
     for player in replay.clients:
@@ -52,6 +68,9 @@ def initiatePlayers(replay):
         players.append(obj)
     return players
 
+# 
+# Fetches which indici the player with the name is in the players array
+#
 def getplayerNumber(name,players):
 
     for i in range(0,len(players) - 1):
@@ -60,13 +79,19 @@ def getplayerNumber(name,players):
 
     return -1
 
+#
+# Initiates the data vector to be returned based on the player 
+# data that we recieve we follow the template above( Line 10 )
+#
 def initiateDataVector(player):
 
     vec = []
     k = 6
+    # Creates a vector of 0s with a size of k
     for i in range(0,k):
         vec.append(0)
 
+    # Sets the given fraction of the player to 1
     if player.protoss == 1:
         vec[1] = 1
     if player.zerg == 1:
@@ -74,11 +99,18 @@ def initiateDataVector(player):
     if player.terran == 1:
         vec[3] = 1
 
+    # Sets the given result of the replay for the 
+    # certain player to a  1 or a 0
     if player.result == 1:
         vec[len(vec) - 1 ] = 1
 
+    # Merges all the values of the vector
     return vec[:]
 
+#
+# This function recieves the trackerEvents subtrack 
+# and calls respective functions to collect the 
+#
 def getTrackerEvents(data, comments, players, trackerEvents):
 
     for tracker in trackerEvents:
