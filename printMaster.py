b6871dffd9b40b8ea494b7a047b91f40d0bfeb2e
aakaashkapoor <aakaash1308@gmail.com>
2018-06-04 15:45:08 -0700

    print master overwritten

--- printMaster.py
+++ printMaster.py
@@ -1,13 +1,18 @@
-import parsing.master as master
+import parsing.master as parser
 import sys
 import printDir.animatePrint as animate
 import printDir.printHelper as helper
 import SVM.svmHandler as svmHandler
 
-# commentOnList = [[18, 'Trentos', 'Probe', 'UnitBornEvent'], [19, 'Onion', 'Probe', 'UnitBornEvent'], [35, 'Trentos', 'Probe', 'UnitBornEvent'], [36, 'Onion', 'Probe', 'UnitBornEvent']]
-commentOnList = master.getPrintData()
-hotVectorData = master.getHotVectorData()
 
+DIR_NAME = "workingReplays"
+DIR_SEPARATOR = '/'
+TEST_REPLAY = "workingReplays/OneSideDominates.SC2Replay"
+# commentOnList = [[18, 'Trentos', 'Probe', 'UnitBornEvent'], [19, 'Onion', 'Probe', 'UnitBornEvent'], [35, 'Trentos', 'Probe', 'UnitBornEvent'], [36, 'Onion', 'Probe', 'UnitBornEvent']]
+
+#
+#
+#
 def comment(simpleEvents):
     timeInSec = 1
     for event in commentOnList:
@@ -26,17 +31,44 @@ def getLine(event):
         return event[1] + ' ' + event[2] + ' ' + event[3]
 
 
-def getSVMResults(hotVectorData):
+
+#
+# going through the replay directory and train through replays
+#
+def trainSVM():
+
+    # initialize the variables
+    xTrainData =[[]]
+    yTrainData = []
+
+    # going through the directory
+    for filename in listdir(DIR_NAME):
+
+        # Setting right path to filename
+        filname = DIR_NAME + DIR_SEPARATOR + filename
+
+        # Not training with the test replay
+        if(filename != TEST_REPLAY):
+            hotVectorData = parser.getTrainData(filename)
+            xTempTrainData, yTempTrainData = svmHandler.callTrainSVM(hotVectorData)
+            xTrainData += [xTempTrainData]
+            yTrainData += [yTrainData]
+
+    svmHandler.callTrainReplays(xTrainData[1:], yTrainData)
+
+
+#
+#
+#.MAIN FUNCTION THAT IS GOING TO DO EVERYTHING
+#
+#
+def runProject():
+    commentOnList = parser.getPrintData(TEST_REPLAY)
+    hotVectorData = parser.getHotVectorData(TEST_REPLAY)
     yOut1,yOut2 = svmHandler.callTestSVM(hotVectorData)
+    print("Player 1 : ",yOut1)
+    print("Player 2 : ", yOut2)
+    comment(commentOnList)
 
-    print yOut1
-    print yOut2
-
-
-def trainSVM(hotVectorData):
-    xTrainData, yTrainData = svmHandler.callTrainSVM(hotVectorData)
-    svmHandler.callTrainReplays(xTrainData,yTrainData)
-
-# comment(commentOnList)
-trainSVM(hotVectorData)
-getSVMResults(hotVectorData)
+trainSVM()
+runProject()
